a3c50ce1a8327b56ebf7584fbf22197f6c8443b7
Nick Thompson <ncthom91@gmail.com>
2023-11-08 07:36:31 -0500

    Paths fix for windows again

--- scripts/build-native.mjs
+++ scripts/build-native.mjs
@@ -3,16 +3,14 @@
 
 let rootDir = await path.resolve(__dirname, '..');
 let buildDir = await path.join(rootDir, 'native', 'build', 'scripted');
-let outDir = await path.join(rootDir, 'native', 'build', 'out');
 
 echo(`Root directory: ${rootDir}`);
 echo(`Build directory: ${buildDir}`);
 
 await fs.ensureDir(buildDir);
-await fs.ensureDir(outDir);
 
 cd(buildDir);
 
-await $`cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${outDir}" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 ../..`;
+await $`cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 ../..`;
 await $`cmake --build . --config Release -j 4`;
 await $`cmake --install . --component plugin`;
